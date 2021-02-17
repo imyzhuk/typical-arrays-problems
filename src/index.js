63aa23f7e40e9fa8e40eddd5a02ac2570ac7e794
imyzhuk <imyzhuk@gmail.com>
2021-02-17 20:26:26 +0300

    feat: add 3 functions.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array = []) {
+    if (array.length > 0) {
+        var num = array[0];
+        for (var i = 0; i < array.length; i++) {
+            if (num > array[i]) {
+                num = array[i];
+            }
+        }
+        return num;
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array = []) {
+    if (array.length > 0) {
+        var num = array[0];
+        for (var i = 0; i < array.length; i++) {
+            if (num < array[i]) {
+                num = array[i];
+            }
+        }
+        return num;
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array = []) {
+    if (array.length > 0) {
+        var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
+    return 0;
+};
